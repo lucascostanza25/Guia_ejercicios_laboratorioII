180394e5c8d4b361df6fa4e1c414e906fc9ab24d
Lucas <lucascostanza55@gmail.com>
2022-08-25 02:00:50 -0300

    Ejercicios clase 1

---
+++ C1_Ejercicio01/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Ingresar 5 números por consola, guardándolos en una variable escalar. 
+ * Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.*/
+
+using System;
+
+namespace C1_Ejercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Clase 1 - ejercicio 01";
+            int numeroIngresado = 0;
+            int numeroMaximo = 0, numeroMinimo = 0, acumulador = 0;
+            float promedio = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Ingrese un numero ({i}/5): ");
+                numeroIngresado = int.Parse(Console.ReadLine());
+
+                if (i == 1)
+                {
+                    numeroMaximo = numeroIngresado;
+                    numeroMinimo = numeroIngresado;
+                }
+
+                if (numeroMaximo < numeroIngresado)
+                {
+                    numeroMaximo = numeroIngresado;
+                }
+
+                if (numeroMinimo > numeroIngresado)
+                {
+                    numeroMinimo = numeroIngresado;
+                }
+
+                acumulador += numeroIngresado;
+            }
+
+            promedio = (float)acumulador / 5;
+
+            Console.Write($"El numero maximo es: {numeroMaximo}\n" +
+                          $"El numero minimo es {numeroMinimo}\n" +
+                          $"El promedio es {promedio}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C1_Ejercicio02/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Ingresar un número y mostrar el cuadrado y el cubo del mismo. 
+ * Se debe validar que el número sea mayor que cero, caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!".*/
+
+using System;
+
+namespace C1_Ejercicio02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Clase 1 - Ejercicio 02";
+            double numeroIngresado = 0;
+            double cuadrado = 0, cubo = 0;
+
+            Console.Write("Ingrese un numero: ");
+            numeroIngresado = int.Parse((Console.ReadLine()));
+
+            while (numeroIngresado == 0)
+            {
+                Console.Write("ERROR. ¡Reingresar número!: ");
+                numeroIngresado = int.Parse((Console.ReadLine()));
+            }
+
+            cuadrado = Math.Pow(numeroIngresado, 2);
+            cubo = Math.Pow(numeroIngresado, 3);
+
+            Console.Write($"El cuadrado del numero {numeroIngresado} es: {cuadrado}\n" +
+                          $"El cubo del numero {numeroIngresado} es: {cubo}");
+            Console.ReadLine();
+        }
+    }
+}
